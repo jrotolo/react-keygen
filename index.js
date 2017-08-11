e7adb18df44703550a7c70b702b474b4b32a4616
jrotolo <jrotolo91@gmail.com>
2017-08-10 19:09:20 -0500

    add test script to package.json

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var React = require('react')
 var HashAlgorithms = require('./hash')
 
+// Maps over the given data to be listed out and automatically creates unique react keys
+// Unique react keys are created by hashing the data blob used to render the list item.
+// using reactKeyMap can be used instead of Array.map when rendering out children
+// DOM elements that would require unique react keys. Never worry about creating and
+// setting react keys again.
 var reactKeyMap = function(data, fn) {
   let len = data.length
   let results = []
