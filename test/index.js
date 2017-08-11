e5e58f6528507362a165c419929c242a03f1d5fd
jrotolo <jrotolo91@gmail.com>
2017-08-10 20:06:11 -0500

    added more tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var React = require('react')
 var should = require('chai').should();
 var reactKeyMap = require('../index');
+var HashAlgorithms = require('../hash');
 
 describe('#reactKeyMap', function() {
   it('creates a mapping of the old data', function() {
@@ -19,5 +20,24 @@ describe('#reactKeyMap', function() {
     })
 
     count.should.equal(data.length)
+  });
+
+  it('has a valid fnv32 hash key set', function() {
+    var data = [{foo: 'bar'}, {baz: 'spaz'}];
+    var checkSum = HashAlgorithms.fnv32(JSON.stringify(data[0]));
+    var mappedList = reactKeyMap(data, function(obj) {
+      return React.createElement('li', {data: obj});
+    });
+
+    mappedList[0].key.should.equal(checkSum.toString());
+  });
+
+  it('creates a unique hash key', function() {
+    var data = [{foo: 'bar'}, {baz: 'spaz'}];
+    var mappedList = reactKeyMap(data, function(obj) {
+      return React.createElement('li', {data: obj});
+    });
+
+    mappedList[0].key.should.not.equal(mappedList[1].key);
   });
 });
